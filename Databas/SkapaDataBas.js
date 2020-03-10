417be8bdb1de95b03a0fee3800903a70e353c61a
EmilAhlroth <46751805+EmilAhlroth@users.noreply.github.com>
2020-03-10 10:39:45 +0100

    Design is done
    pec

---
+++ Databas/SkapaDataBas.js
@@ -0,0 +1,18 @@
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+host: "localhost",
+user: "root",
+password: ""
+});
+
+
+// Skapar en databas som heter Bilder. Den ska innehålla alla Bilder.
+con.connect(function(err) {
+if (err) throw err;
+console.log("Lyckades!");
+con.query("CREATE DATABASE Bilder", function (err, result) {
+if (err) throw err;
+console.log("Database Skapades");
+});
+});
